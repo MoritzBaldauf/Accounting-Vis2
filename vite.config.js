493b25312ebe7b6805ca773b15c0c557a4757781
Moritz <moritz.baldauf24@gmail.com>
2024-11-10 20:04:45 +0100

    Fix in publishing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/MoritzBaldauf/Accounting-Vis2/settings/pages', // Replace with your repository name
 })
