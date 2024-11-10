c8d027f6d212078aa334be1a3a79dc0e52a4b667
Moritz <moritz.baldauf24@gmail.com>
2024-11-10 20:10:11 +0100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/MoritzBaldauf/Accounting-Vis2/settings/pages', // Replace with your repository name
+  base: './', // Changed to relative path
+  build: {
+    outDir: 'dist',
+    assetsDir: '.',
+  }
 })
